ea28acd34be9621b305c52e013838ea0273fe61a
NGrant <niall.grant1992@gmail.com>
2017-11-02 22:18:13 +0000

    added tweets.js/tweet(), currently just checks if tweet field is empty or not

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -7,7 +7,6 @@ const Joi = require("joi");
 
 exports.home = {
   handler: function(request, reply) {
-
     User.findOne({ email: request.auth.credentials.loggedInUser })
       .then(user => {
         return Tweet.find({})
@@ -33,5 +32,42 @@ exports.home = {
         Logger.info(err);
         reply.redirect("/");
       });
+  }
+};
+
+exports.tweet = {
+  validate: {
+    payload: {
+      tweetText: Joi.string().required()
+    },
+
+    failAction: function(request, reply, source, error) {
+      User.findOne({ email: request.auth.credentials.loggedInUser })
+        .then(user => {
+          return Tweet.find({})
+            .populate("user")
+            .then(tweets => {
+              return [tweets, user];
+            })
+            .then(results => {
+              return User.find({}).then(userlist => {
+                return [results[0], results[1], userlist];
+              });
+            });
+        })
+        .then(result => {
+          reply.view("home", {
+            title: "Post a Tweet",
+            tweets: result[0],
+            user: result[1],
+            userlist: result[2],
+            errors: error.data.details
+          });
+        })
+        .catch(err => {
+          Logger.info(err);
+          reply.redirect("/");
+        });
+    }
   }
 };
