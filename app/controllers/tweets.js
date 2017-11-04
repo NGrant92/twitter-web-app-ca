1685fd4cace5a495d9d2e8a94fae22c417985fd3
NGrant <niall.grant1992@gmail.com>
2017-11-04 11:25:53 +0000

    deleteTweeSet fully implemented

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -7,38 +7,38 @@ const Joi = require("joi");
 exports.home = {
   handler: function(request, reply) {
     User.findOne({ email: request.auth.credentials.loggedInUser })
-      .then(user => {
-        return Tweet.find({})
-          .populate("user")
-          .then(tweets => {
-            let myTweets = [];
-            for (let i = 0; i < tweets.length; i++) {
-              let tweet = tweets[i];
-              if (tweet.user.id === user.id) {
-                myTweets.push(tweet);
-              }
-            }
-            return [tweets.reverse(), myTweets.reverse(), user];
-          })
-          .then(results => {
-            return User.find({}).then(userlist => {
-              return [results[0], results[1], results[2], userlist];
-            });
+        .then(user => {
+          return Tweet.find({})
+              .populate("user")
+              .then(tweets => {
+                let myTweets = [];
+                for (let i = 0; i < tweets.length; i++) {
+                  let tweet = tweets[i];
+                  if (tweet.user.id === user.id) {
+                    myTweets.push(tweet);
+                  }
+                }
+                return [tweets.reverse(), myTweets.reverse(), user];
+              })
+              .then(results => {
+                return User.find({}).then(userlist => {
+                  return [results[0], results[1], results[2], userlist];
+                });
+              });
+        })
+        .then(result => {
+          reply.view("home", {
+            title: "Home",
+            tweets: result[0],
+            myTweets: result[1],
+            user: result[2],
+            userlist: result[3]
           });
-      })
-      .then(result => {
-        reply.view("home", {
-          title: "Home",
-          tweets: result[0],
-          myTweets: result[1],
-          user: result[2],
-          userlist: result[3]
+        })
+        .catch(err => {
+          Logger.info(err);
+          reply.redirect("/");
         });
-      })
-      .catch(err => {
-        Logger.info(err);
-        reply.redirect("/");
-      });
   }
 };
 
@@ -48,16 +48,16 @@ exports.tweet = {
     const tweet = new Tweet(request.payload);
 
     User.findOne({ email: userEmail })
-      .then(user => {
-        tweet.user = user._id;
-        return tweet.save();
-      })
-      .then(newTweet => {
-        reply.redirect("/home");
-      })
-      .catch(err => {
-        reply.redirect("/");
-      });
+        .then(user => {
+          tweet.user = user._id;
+          return tweet.save();
+        })
+        .then(newTweet => {
+          reply.redirect("/home");
+        })
+        .catch(err => {
+          reply.redirect("/");
+        });
   }
 };
 
@@ -67,37 +67,37 @@ exports.viewUser = {
 
     if (paramEmail !== request.auth.credentials.loggedInUser) {
       User.findOne({ email: paramEmail })
-        .then(user => {
-          return Tweet.find({})
-            .populate("user")
-            .then(tweets => {
-              let userTweets = [];
-              for (let i = 0; i < tweets.length; i++) {
-                let tweet = tweets[i];
-                if (tweet.user.id === user.id) {
-                  userTweets.push(tweet);
-                }
-              }
-              return [userTweets.reverse(), user];
-            })
-            .then(results => {
-              return User.find({}).then(userlist => {
-                return [results[0], results[1], userlist];
-              });
+          .then(user => {
+            return Tweet.find({})
+                .populate("user")
+                .then(tweets => {
+                  let userTweets = [];
+                  for (let i = 0; i < tweets.length; i++) {
+                    let tweet = tweets[i];
+                    if (tweet.user.id === user.id) {
+                      userTweets.push(tweet);
+                    }
+                  }
+                  return [userTweets.reverse(), user];
+                })
+                .then(results => {
+                  return User.find({}).then(userlist => {
+                    return [results[0], results[1], userlist];
+                  });
+                });
+          })
+          .then(result => {
+            reply.view("viewuser", {
+              title: result[1].firstName + "'s Profile",
+              tweets: result[0],
+              user: result[1],
+              userlist: result[2]
             });
-        })
-        .then(result => {
-          reply.view("viewuser", {
-            title: result[1].firstName + "'s Profile",
-            tweets: result[0],
-            user: result[1],
-            userlist: result[2]
+          })
+          .catch(err => {
+            Logger.info(err);
+            reply.redirect("/");
           });
-        })
-        .catch(err => {
-          Logger.info(err);
-          reply.redirect("/");
-        });
     } else {
       reply.redirect("/home");
     }
@@ -109,31 +109,29 @@ exports.deleteAllTweets = {
     let userEmail = request.auth.credentials.loggedInUser;
 
     User.findOne({ email: userEmail })
-      .then(user => {
-        Logger.info(user.id);
-        Tweet.remove({ user: user.id }).then(result => {
-          reply.redirect("/home");
+        .then(user => {
+          Logger.info(user.id);
+          Tweet.remove({ user: user.id }).then(result => {
+            reply.redirect("/home");
+          });
+        })
+        .catch(err => {
+          Logger.info(err);
+          reply.redirect("/");
         });
-      })
-      .catch(err => {
-        Logger.info(err);
-        reply.redirect("/");
-      });
   }
 };
 
 exports.deleteTweetSet = {
   handler: function(request, reply) {
     let tweetSet = Object.keys(request.payload);
-    let tweetid;
-    for (tweetid in tweetSet) {
-      Tweet.findOneAndRemove({ id: tweetid })
-        .then(result => {
-          Logger.info("Tweet Removed");
-        })
-        .catch(err => {
-          Logger.info(err);
-        });
+
+    for (let i = 0; i < tweetSet.length; i++) {
+      Tweet.remove({ _id: tweetSet[i] }).then(result =>{
+        console.log("Tweet Removed: " + tweetSet[i]);
+      }).catch(err => {
+        console.log(err);
+      });
     }
     reply.redirect("/home");
   }
