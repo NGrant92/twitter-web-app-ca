e18cb97a45d936c0c1de45c2183ca195cb7ab1be
NGrant <niall.grant1992@gmail.com>
2017-11-04 11:44:35 +0000

    refined tweets.js/home() and viewUser()

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -7,38 +7,38 @@ const Joi = require("joi");
 exports.home = {
   handler: function(request, reply) {
     User.findOne({ email: request.auth.credentials.loggedInUser })
-        .then(user => {
-          return Tweet.find({})
+      .then(foundUser => {
+        return Tweet.find({})
+          .populate("user")
+          .then(tweets => {
+            return [tweets.reverse(), foundUser];
+          })
+          .then(results => {
+            return Tweet.find({ user: results[1] })
               .populate("user")
-              .then(tweets => {
-                let myTweets = [];
-                for (let i = 0; i < tweets.length; i++) {
-                  let tweet = tweets[i];
-                  if (tweet.user.id === user.id) {
-                    myTweets.push(tweet);
-                  }
-                }
-                return [tweets.reverse(), myTweets.reverse(), user];
-              })
-              .then(results => {
-                return User.find({}).then(userlist => {
-                  return [results[0], results[1], results[2], userlist];
-                });
+              .then(myTweets => {
+                return [results[0],results[1], myTweets.reverse()];
               });
-        })
-        .then(result => {
-          reply.view("home", {
-            title: "Home",
-            tweets: result[0],
-            myTweets: result[1],
-            user: result[2],
-            userlist: result[3]
+          })
+          .then(results => {
+            return User.find({}).then(userlist => {
+              return [results[0], results[1], results[2], userlist];
+            });
           });
-        })
-        .catch(err => {
-          Logger.info(err);
-          reply.redirect("/");
+      })
+      .then(result => {
+        reply.view("home", {
+          title: "Home",
+          tweets: result[0],
+          user: result[1],
+          myTweets: result[2],
+          userlist: result[3]
         });
+      })
+      .catch(err => {
+        Logger.info(err);
+        reply.redirect("/");
+      });
   }
 };
 
@@ -48,16 +48,16 @@ exports.tweet = {
     const tweet = new Tweet(request.payload);
 
     User.findOne({ email: userEmail })
-        .then(user => {
-          tweet.user = user._id;
-          return tweet.save();
-        })
-        .then(newTweet => {
-          reply.redirect("/home");
-        })
-        .catch(err => {
-          reply.redirect("/");
-        });
+      .then(user => {
+        tweet.user = user._id;
+        return tweet.save();
+      })
+      .then(newTweet => {
+        reply.redirect("/home");
+      })
+      .catch(err => {
+        reply.redirect("/");
+      });
   }
 };
 
@@ -67,37 +67,30 @@ exports.viewUser = {
 
     if (paramEmail !== request.auth.credentials.loggedInUser) {
       User.findOne({ email: paramEmail })
-          .then(user => {
-            return Tweet.find({})
-                .populate("user")
-                .then(tweets => {
-                  let userTweets = [];
-                  for (let i = 0; i < tweets.length; i++) {
-                    let tweet = tweets[i];
-                    if (tweet.user.id === user.id) {
-                      userTweets.push(tweet);
-                    }
-                  }
-                  return [userTweets.reverse(), user];
-                })
-                .then(results => {
-                  return User.find({}).then(userlist => {
-                    return [results[0], results[1], userlist];
-                  });
-                });
-          })
-          .then(result => {
-            reply.view("viewuser", {
-              title: result[1].firstName + "'s Profile",
-              tweets: result[0],
-              user: result[1],
-              userlist: result[2]
+        .then(foundUser => {
+          return Tweet.find({ user: foundUser })
+            .populate("user")
+            .then(userTweets => {
+              return [userTweets.reverse(), foundUser];
+            })
+            .then(results => {
+              return User.find({}).then(userlist => {
+                return [results[0], results[1], userlist];
+              });
             });
-          })
-          .catch(err => {
-            Logger.info(err);
-            reply.redirect("/");
+        })
+        .then(result => {
+          reply.view("viewuser", {
+            title: result[1].firstName + "'s Profile",
+            tweets: result[0],
+            user: result[1],
+            userlist: result[2]
           });
+        })
+        .catch(err => {
+          Logger.info(err);
+          reply.redirect("/");
+        });
     } else {
       reply.redirect("/home");
     }
@@ -109,16 +102,16 @@ exports.deleteAllTweets = {
     let userEmail = request.auth.credentials.loggedInUser;
 
     User.findOne({ email: userEmail })
-        .then(user => {
-          Logger.info(user.id);
-          Tweet.remove({ user: user.id }).then(result => {
-            reply.redirect("/home");
-          });
-        })
-        .catch(err => {
-          Logger.info(err);
-          reply.redirect("/");
+      .then(user => {
+        Logger.info(user.id);
+        Tweet.remove({ user: user.id }).then(result => {
+          reply.redirect("/home");
         });
+      })
+      .catch(err => {
+        Logger.info(err);
+        reply.redirect("/");
+      });
   }
 };
 
@@ -127,11 +120,13 @@ exports.deleteTweetSet = {
     let tweetSet = Object.keys(request.payload);
 
     for (let i = 0; i < tweetSet.length; i++) {
-      Tweet.remove({ _id: tweetSet[i] }).then(result =>{
-        console.log("Tweet Removed: " + tweetSet[i]);
-      }).catch(err => {
-        console.log(err);
-      });
+      Tweet.remove({ _id: tweetSet[i] })
+        .then(result => {
+          console.log("Tweet Removed: " + tweetSet[i]);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
     reply.redirect("/home");
   }
